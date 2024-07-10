c7ee13bdb07a275344364496a1bb1a10e2de05a5
AlifAw <alifaddarisalam8@gmail.com>
2024-07-10 22:22:02 +0700

    2

--- menu.php
+++ menu.php
@@ -30,9 +30,7 @@
       <div class="menu-flip-card-back">
         <div class="menu-text">
           <h3 class="menu-text-title">Bakso Urat</h3>
-          <p class="about-text">
-            Bakso urat adalah variasi bakso yang terkenal di Indonesia, dikenal karena teksturnya yang khas.
-          </p>
+          <br>
           <h3 class="menu-text-title">Rp25.000</h3>
           <!-- Form Pemesanan -->
           <form class="order-form">
@@ -55,10 +53,9 @@
             <img src="folder gambar/bakso telur.jpg" alt="Avatar" class="menu-image">
           </div>
           <div class="menu-flip-card-back">
-            <div class="menu-text"><h3 class="menu-text-title">Bakso Telur</h3>
-              <p class="about-text">
-                Bakso telur adalah jenis bakso yang di dalamnya terdapat telur rebus.   
-              </p>
+            <div class="menu-text">
+                <h3 class="menu-text-title">Bakso Telur</h3>
+                <br>
               <h3 class="menu-text-title">Rp30.000</h3>
               <form class="order-form">
                 <label for="quantity-bakso-urat">Jumlah Pesanan:</label>
@@ -79,10 +76,9 @@
             <img src="folder gambar/bakso lava.jpg" alt="Avatar" class="menu-image">
           </div>
           <div class="menu-flip-card-back">
-            <div class="menu-text"><h3 class="menu-text-title">Bakso Lava</h3>
-              <p class="about-text">
-                Bakso lava adalah inovasi modern dalam dunia bakso dan banyak peminatnya. 
-              </p>
+            <div class="menu-text">
+                <h3 class="menu-text-title">Bakso Lava</h3>
+                <br>
               <h3 class="menu-text-title">Rp35.000</h3>
               <form class="order-form">
                 <label for="quantity-bakso-urat">Jumlah Pesanan:</label>
@@ -103,10 +99,9 @@
             <img src="folder gambar/bakso iga.jpg" alt="Avatar" class="menu-image">
           </div>
           <div class="menu-flip-card-back">
-            <div class="menu-text"><h3 class="menu-text-title">Bakso Iga Sapi</h3>
-              <p class="about-text">
-                Bakso iga sapi adalah bakso yang menggunakan iga sapi sebagai bahan utamanya, selain daging sapi giling.
-              </p>
+            <div class="menu-text">
+                <h3 class="menu-text-title">Bakso Iga Sapi</h3>
+                <br>
               <h3 class="menu-text-title">Rp40.000</h3>
               <form class="order-form">
                 <label for="quantity-bakso-urat">Jumlah Pesanan:</label>
